a7b5362b460a33cbf96ef3fe4d1b73c4249dd074
1815@first.local <1815@first.local>
2019-10-04 11:15:12 +0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-Я постигаю JavaScript
+var node = document.getElementById("convert");
+
+var name = node.id;
+node.innerHTML = "В F";
+
+var button = document.getElementById("convert");
+var count = 0;
+
+function print() {
+    count = count + 1;
+    console.log("Кол-во нажатий кнопки: " + count);
+}
+
+button.addEventListener("click", print);
+
+var input = document.getElementById("firld");
+x = Number(input.value);
+x + 1;
